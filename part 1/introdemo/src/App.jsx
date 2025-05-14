f6a5e5a5814b45649643ae610ea19e05c8ec7b85
Iykra <rifki.banner@gmail.com>
2025-05-14 11:21:48 +0700

    part 1

--- part 1/introdemo/src/App.jsx
+++ part 1/introdemo/src/App.jsx
@@ -1,20 +1,45 @@
-const Hello = (props) => {
-  console.log(props)
-  return(
-    <div>
-      {/* Nama variabel yang di-pass bebas hgak harus props */}
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+import { useState } from 'react'
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
 const App = () => {
-  const friends = ['Peter', 'Bob']
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <p>{friends}</p>
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
+    </div>
   )
 }
 
