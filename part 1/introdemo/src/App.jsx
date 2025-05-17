6a492956063a83059d5654016ed439e8532bc324
Iykra <rifki.banner@gmail.com>
2025-05-17 18:33:31 +0700

    Exercises 1.6.-1.14.

--- part 1/introdemo/src/App.jsx
+++ part 1/introdemo/src/App.jsx
@@ -1,59 +1,29 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Display = (props) => <div>{props.value}</div>
 
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the button
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = (props) => {
-  console.log('props is ', props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick} > {text}</button>
-  )
-}
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(left + 1)
-    setTotal(updatedLeft + right)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(right + 1)
-    setTotal(updatedRight + left)
-  }
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text='thousand' />
+      <Button onClick={() => setToValue(0)} text='reset' />
+      <Button onClick={() => setToValue(value + 1)} text='increase' />
     </div>
   )
 }
---
+++ part 1/unicafe/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Display = ({ value }) => {
+  return (
+    <div>
+      <h1>{value}</h1>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={good + neutral + bad} />
+          <StatisticsLine text="average" value={((good - bad) / (good + neutral + bad))} />
+          <StatisticsLine text="positive" value={((good / (good + neutral + bad)) * 100)} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Display value="give feedback" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Display value="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+export default App
