47ddc860ce52b2522f30cbfa53eba71b5b38e8ee
Iykra <rifki.banner@gmail.com>
2025-05-19 12:23:44 +0700

    part 2 started

--- part 2/courseinfo/src/App.jsx
+++ part 2/courseinfo/src/App.jsx
@@ -1,53 +1,20 @@
-const Header = (props) => {
-  console.log(props.course)
-  return <h1>{props.course}</h1>
-  
-}
-
-const Part = (props) => {
+const Note = ({ note }) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <li>{note.content}</li>
   )
 }
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+const App = ({ notes }) => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map((note) =>
+            <Note note={note} key={note.id} />
+          )}
+      </ul>
     </div>
   )
 }
