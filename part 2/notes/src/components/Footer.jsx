b3ae747ecad58a6ee679118045ec945bacdf1d2f
Iykra <rifki.banner@gmail.com>
2025-05-24 20:30:03 +0700

    part 2e

---
+++ part 2/notes/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic'
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <p>
+                Note app, Department of Computer Science, University of Helisnki 2025
+            </p>
+        </div>
+    )
+}
+
+export default Footer
