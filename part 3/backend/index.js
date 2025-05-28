6f79e8e1b6c1895a4326a7b00ed19b34cf0dcfa3
Iykra <rifki.banner@gmail.com>
2025-05-28 14:28:13 +0700

    Exercises 3.7.-3.8.

--- part 3/backend/index.js
+++ part 3/backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
 
 let notes = [
     {
@@ -20,6 +19,18 @@ let notes = [
         important: true
     }
 ]
+
+// melakukan pendefinisian middleware -> umumnya penempatannya di sebelum routes 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -72,6 +83,13 @@ app.post('/api/notes', (request, response) => {
 
     response.json(note)
 })
+
+// mendefinisikan middleware yang bisa didefinisikan setelah routes
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
