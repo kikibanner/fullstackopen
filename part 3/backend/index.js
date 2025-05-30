32405e78c5d3be5a2135d4b3c5d6e632c46775be
Iykra <rifki.banner@gmail.com>
2025-05-30 23:30:24 +0700

    just keeping it green :( :)

--- part 3/backend/index.js
+++ part 3/backend/index.js
@@ -93,6 +93,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
