739cf7c374d44267c6964b9898cfbd84cdae38e7
Iykra <rifki.banner@gmail.com>
2025-06-01 13:24:51 +0700

    phonebook-uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 
 app.use(express.static('dist'))
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json())
 
