4be96aa07dde496e9116550311e27faae2a7efe4
Iykra <rifki.banner@gmail.com>
2025-06-04 11:10:17 +0700

    phonebook-uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -22,28 +22,28 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(morgan('tiny'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         "id": "1",
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": "2",
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": "3",
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": "4",
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/info', (request, response) => {
     response.send(`
@@ -82,9 +82,9 @@ const generateId = () => {
     return String(Math.floor(Math.random() * 1_000_000_000))
 }
 
-const searchDuplicate = (name) => {
-    return persons.filter(person => person.name == name).length
-}
+// const searchDuplicate = (name) => {
+//     return persons.filter(person => person.name == name).length
+// }
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -95,11 +95,11 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (searchDuplicate(body.name) !== 0) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
+    // if (searchDuplicate(body.name) !== 0) {
+    //     return response.status(400).json({
+    //         error: "name must be unique"
+    //     })
+    // }
 
     const person = new Person({
         name: body.name,
