4c0ef6bb3f3d3e34712b9e2010e4477acdc79803
Iykra <rifki.banner@gmail.com>
2025-06-24 14:04:55 +0700

    Exercise 4.15: Blog List Expansion, step 3

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    // const user = await User.findById(body.userId)
+
+    if (!user) {
+        return response.status(400).json({ error: 'userId missing or not valid' })
+    }
 
     if (!body.url || !body.title) {
         return response.status(400).json({ error: 'title or url missing' })
@@ -18,9 +39,12 @@ blogsRouter.post('/', async (request, response) => {
         author: body.author,
         url: body.url,
         likes: body.likes || 0,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     // console.log('Saved blog:', savedBlog)
     response.status(201).json(savedBlog)
 })
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -15,9 +15,25 @@ const errorHandler = (error, request, response, next) => {
     console.log(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({
+            error: 'malformatted id'
+        })
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).json({
+            error: error.message
+        })
+    } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+        return response.status(400).json({
+            error: 'expected `username` to be unique'
+        })
+    } else if (error.name === 'JsonWebToken') {
+        return response.status(401).json({
+            error: 'token invalid'
+        })
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({
+            error: 'token expired'
+        })
     }
 
     next(error)
